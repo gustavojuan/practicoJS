e79832bdc8521aa1727323ca7e0646d99c6a36b5
gustavojuan <gustavojuan@gmail.com>
2023-03-03 11:01:57 +0100

    Leyendo HTML desde JS

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello world!');
+//
+const h1 = document.querySelector("h1");
+const p = document.querySelectorAll("p");
+const parrafito = document.querySelector(".parrafito");
+const pid = document.querySelector("#pid");
+const input = document.querySelector("input");
+
+console.log(h1);
+console.log({h1});
+console.log(p);
+console.log(parrafito);
+console.log(pid);
+console.log(input.value);
+
+console.log({
+  h1,
+  p,
+  parrafito,
+  pid,
+  input,
+});
