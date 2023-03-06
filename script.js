6b78d3f1f9822e9f476aa6c88141f27ef83041ef
gustavojuan <gustavojuan@gmail.com>
2023-03-06 06:00:51 +0100

    Addeventlistener click

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const h1 = document.querySelector("h1");
 const input1 = document.querySelector("#calculo1");
 const input2 = document.querySelector("#calculo2");
-const btn = document.querySelector("btnCalcular");
+const btn = document.querySelector("#btnCalcular");
 const presult = document.querySelector("#result");
 
-function btnOnclick() {
+//mejor legibilidad
+btn.addEventListener('click', btnOnClick );
+
+function btnOnClick() {
   //concatena por default al tratarse de inputs
   let result = Number(input1.value) + Number(input2.value);
   presult.innerText = "Resultado:" + result;
